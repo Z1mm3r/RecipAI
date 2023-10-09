7e936c445045552a40666cab57d8a85fd38abdb4
Z1mm3r <czimmerdev@gmail.com>
2023-10-08 23:33:23 -0400

    design-header-figma

--- recipai-frontend/src/components/MainHeader/index.js
+++ recipai-frontend/src/components/MainHeader/index.js
@@ -2,12 +2,22 @@ import React, { useState, useCallback, useEffect } from 'react'
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
 
 
-
+//Figure out designs for header, should we have a profile badge, etc?
 const MainHeader = () => {
     return (
         <>
-            <Grid2>
-                Header
+            <Grid2 container>
+
+                <Grid2>
+                    Logo
+                </Grid2>
+                <Grid2>
+                    Spacer
+                </Grid2>
+                <Grid2>
+                    Right Side Items
+                </Grid2>
+
             </Grid2>
         </>
     )
